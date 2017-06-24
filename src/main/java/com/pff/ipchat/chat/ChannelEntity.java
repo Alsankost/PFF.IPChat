e4b8d144087dd13abed87162f20a85a839cbf950
alex <alsankost@gmail.com>
2017-06-24 11:31:16 +0300

    ChannelEntity first

--- src/main/java/com/pff/ipchat/chat/ChannelEntity.java
+++ src/main/java/com/pff/ipchat/chat/ChannelEntity.java
@@ -5,4 +5,22 @@ package com.pff.ipchat.chat;
  * From com.pff.ipchat.chat
  */
 public class ChannelEntity {
+	private String sid = null; //Specific id
+	private String channel_version; //String for version channel in CVM
+	
+	public ChannelEntity(String sid, String cv)
+	{
+		this.sid = sid;
+		this.channel_version = cv;
+	}
+	
+	public String getSID()
+	{
+		return sid;
+	}
+	
+	public String getChannelVersion()
+	{
+		return channel_version;
+	}
 }
