696108d00f26ad81d890c4f703eba3b178bde902
Good-Pudge <arsenylyashenko.3@gmail.com>
2017-06-24 12:23:21 +0300

    Work with GUI

--- src/main/java/com/pff/ipchat/Main.java
+++ src/main/java/com/pff/ipchat/Main.java
@@ -1,10 +1,16 @@
-package com.pff.ipchat;/**
+package com.pff.ipchat;
+/**
  * Created by Good_Pudge(Senya) on 24.06.2017.
  * From com.pff.ipchat
  */
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -13,7 +19,14 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+        BorderPane mainForm = FXMLLoader.load(Main.class.getResource("view/fxml/MainForm.fxml"));
 
+        Scene scene = new Scene(mainForm);
+
+        primaryStage.setScene(scene);
+        primaryStage.setWidth(mainForm.getWidth());
+        primaryStage.setHeight(mainForm.getHeight());
+        primaryStage.show();
     }
 }
---
+++ src/main/java/com/pff/ipchat/view/ChatFormController.java
@@ -0,0 +1,17 @@
+package com.pff.ipchat.view;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Good_Pudge(Senya) on 24.06.2017.
+ * From com.pff.ipchat.view
+ */
+public class ChatFormController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
